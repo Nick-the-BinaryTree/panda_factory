b15365e282c90b0a3113eff1315c93bc2822e9af
nick11h <nick11hd@gmail.com>
2018-05-16 10:10:43 -0400

    panda component and app.js tweaks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 // @flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './imgs/logo.svg';
 import './App.css';
+import Panda, {Props as PandaType} from './components/Panda.js'
 
 type Props = null
 
 type State = {
-  pandas: Array<Panda>
-}
-
-type Panda = {
-  id: number,
-  name: string,
-  age: number,
-  hobby: string
+  pandas: Array<PandaType>
 }
 
 class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props)
     this.state = {pandas: [
-      {id: 0, name: "Peter", age: 7, hobby: "race car driving"},
-      {id: 1, name: "Paulina", age: 14, hobby: "VR simulations"},
-      {id: 2, name: "Patrick", age: 5, hobby: "fighting crime"}
+      {id: 0, name: "Peter the Panda", age: 7, hobby: "race car driving", img: "1"},
+      {id: 1, name: "Paulina the Panda", age: 14, hobby: "VR simulations", img: "2"},
+      {id: 2, name: "Patrick the Panda", age: 5, hobby: "fighting crime", img: "3"}
     ]}
   }
   render() {
     const pandaComponents = []
 
     for (let i=0; i<this.state.pandas.length; i++)
-      pandaComponents.push(<p>test</p>)
+      pandaComponents.push(<Panda id={this.state.pandas[i].id}
+        name={this.state.pandas[i].name} age={this.state.pandas[i].age}
+        hobby={this.state.pandas[i].hobby} img={this.state.pandas[i].img} />)
 
     return (
       <div className="App">
--- src/components/Panda.js
+++ src/components/Panda.js
@@ -1,1 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+export type Props = {
+  id: number,
+  name: string,
+  age: number,
+  hobby: string,
+  img: string
+}
+
+const Panda = (props: Props) => (
+  <div>
+    <p>Name: {props.name} | Age: {props.age} | Hobby: {props.hobby}</p>
+    <img src={require("../imgs/panda"+props.img+".jpg")} alt="most likely a panda" />
+  </div>
+)
+
+export default Panda
